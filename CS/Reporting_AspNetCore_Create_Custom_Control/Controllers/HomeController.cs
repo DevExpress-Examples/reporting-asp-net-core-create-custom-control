40c26b3edca1dbfe382fcbd5a3301ea01cb2fb0b
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2021-06-03 08:19:17 +0300

    auto sync for version 2020.2

--- CS/Reporting_AspNetCore_Create_Custom_Control/Controllers/HomeController.cs
+++ CS/Reporting_AspNetCore_Create_Custom_Control/Controllers/HomeController.cs
@@ -3,10 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace Reporting_AspNetCore_Create_Custom_Control.Controllers {
     public class HomeController : Controller {
         public IActionResult Index() {
-            return View();
-        }
-
-        public IActionResult Designer() {
             Models.ReportDesignerModel model = new Models.ReportDesignerModel();
             return View(model);
         }
