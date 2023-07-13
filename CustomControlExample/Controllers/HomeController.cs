736b4fcac329caa4928b52f1f0540dbadee205ae
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2023-07-13 17:19:36 +0400

    auto sync for version 2023.1

--- CustomControlExample/Controllers/HomeController.cs
+++ CustomControlExample/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using DevExpress.XtraReports.Web.ReportDesigner;
+using DevExpress.XtraReports.Web.ReportDesigner.Services;
 
 namespace Reporting_AspNetCore_Create_Custom_Control.Controllers {
     public class HomeController : Controller {
-        public IActionResult Index() {
-            Models.ReportDesignerModel model = new Models.ReportDesignerModel();
+        public IActionResult Index([FromServices] IReportDesignerModelBuilder reportDesignerModelBuilder) {
+            ReportDesignerModel model = reportDesignerModelBuilder
+                .Report("TestReport")
+                .CustomControls(typeof(MyControl), typeof(NumericLabel))
+                .BuildModel();
             return View(model);
         }
     }
